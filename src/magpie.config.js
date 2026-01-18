50c276f18b7937e2c4ffab0330fe23af2a105138
Marichka Nadverniuk <116063587+mnadvernyuk@users.noreply.github.com>
2026-01-18 21:27:39 +0100

    Update magpie.config.js for Prolific

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,12 +1,11 @@
 export default {
-  experimentId: '43',
-  serverUrl: 'https://magpie-cogsciprag.fly.dev',
-  socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
-  completionUrl: 'https://...',
-  contactEmail: 'exprag@gmail.com',
-  mode: 'prolific',
-  language: 'en',
-  stimuli: {
-    main: 'stimuli/vignettes.csv'
-  }
+    experimentId: '43’,
+    serverUrl: 'https://magpie-cogsciprag.fly.dev',
+    socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
+    // prolific mode_
+    completionUrl: 'https://app.prolific.com/submissions/complete?cc=CK0WQZY1',
+    contactEmail: 'michael.franke@uni-tuebingen.de',
+    // Either 'debug', 'directLink', 'prolific'_
+    mode: 'prolific',
+    language: 'en'
 };
